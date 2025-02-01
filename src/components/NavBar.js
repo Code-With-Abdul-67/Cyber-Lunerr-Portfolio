0c56377239ce5bb8b81d87dd144a7d355c8cd2ee
XBEAST1 <nusrat.saba609@gmail.com>
2025-02-02 01:26:36 +0500

    Added Resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -42,28 +42,76 @@ export const NavBar = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
             </Nav>
 
             <span className="navbar-text">
               <div className="social-icon">
-
-                <a href="https://www.reddit.com/user/One-Alternative-7613/" target="_blank" rel="noreferrer noopener">
-                <img src={navIcon1} alt="reddit-link" /></a>
-                <a href="mailto:xlunerr76@gmail.com" target="_blank" rel="noreferrer noopener">
-                <img src={navIcon2} alt="mail-link" /></a>
-                <a href="https://github.com/Code-With-Abdul-67" target="_blank" rel="noreferrer noopener">
-                <img src={navIcon3} alt="github-link" /></a>
+                <a
+                  href="https://www.reddit.com/user/One-Alternative-7613/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <img src={navIcon1} alt="reddit-link" />
+                </a>
+                <a
+                  href="mailto:xlunerr76@gmail.com"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <img src={navIcon2} alt="mail-link" />
+                </a>
+                <a
+                  href="https://github.com/Code-With-Abdul-67"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <img src={navIcon3} alt="github-link" />
+                </a>
               </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let's Connect</span></button>
-              </HashLink>
+              <a
+                href="/CV.pdf"
+                className="resumebtn"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Download Resume
+              </a>
+              <button className="vvd">
+                <span></span>
+              </button>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </Router>
-  )
+  );
 }
